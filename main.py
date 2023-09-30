fd907a9af8d63b39cafad50addb5187f3c064c3c
Kent Martin Åsvang <github.com@asvang.no>
2023-09-30 18:12:24 +0200

    basic are in place

---
+++ main.py
@@ -0,0 +1,29 @@
+import time
+import shutil
+from pathlib import Path 
+
+
+def _check_and_deploy_files(service):
+
+    publish_dir = Path(service["publish"]) 
+    public_dir = Path(service["public"])
+    published_files = list(publish_dir.iterdir())
+        
+    if any(published_files):
+        for file in published_files:
+            shutil.move(file, public_dir)
+
+
+def main():
+    from utils import read_service_file
+
+    services = read_service_file("services.yaml") 
+
+    while True:
+        time.sleep(60)
+        for service in services:
+            _check_and_deploy_files(service)
+
+
+if __name__ == "__main__":
+    main()
