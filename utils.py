2e7fcb8ffabc1ec8112effcb5cfabb04b59e484c
Kent Martin Åsvang <github.com@asvang.no>
2023-10-01 21:35:47 +0200

    blah

--- utils.py
+++ utils.py
@@ -1,8 +1,4 @@
-try:
-    import yaml
-except ImportError:
-    print("You need to run 'pip3 install pyyaml'")
-    exit(0)
+import yaml
 
 def read_service_file(config_path):
     with open(config_path, "r") as f:
