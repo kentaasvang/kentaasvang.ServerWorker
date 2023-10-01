af5d19c69d624d23bd4f4e4350b1d0596ca4be0e
Kent Martin Åsvang <github.com@asvang.no>
2023-10-01 21:22:58 +0200

    added check for installed dependency

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
-import yaml
+try:
+    import yaml
+except ImportError:
+    print("You need to run 'pip3 install pyyaml'")
+    exit(0)
 
 def read_service_file(config_path):
     with open(config_path, "r") as f:
