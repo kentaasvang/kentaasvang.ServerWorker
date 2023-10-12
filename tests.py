5d4acb87346088fe0006aa0435982bedcb66c4bb
Kent Martin Åsvang <github.com@asvang.no>
2023-10-12 17:07:47 +0200

    successfully removing old items - including folders

--- tests.py
+++ tests.py
@@ -6,13 +6,11 @@ import shutil
 # disable for tests
 import logging
 logging.disable()
-import logging
-
 
 from main import _check_and_deploy_files
 from utils import read_service_file
 
-class TestCheckAndDeploy(unittest.TestCase):
+class TestServerWorker(unittest.TestCase):
 
     def setUp(self):
         self.tmp_publish_dir = tempfile.mkdtemp(prefix="publish_") 
@@ -26,7 +24,9 @@ class TestCheckAndDeploy(unittest.TestCase):
         if os.path.exists(self.tmp_public_dir):
             shutil.rmtree(self.tmp_public_dir)
 
-    def test_check_and_deploy_file(self):
+    def test_items_are_moved_from_publish_dir(self):
+
+        self.tmp_published_folder = tempfile.mkdtemp(dir=self.tmp_publish_dir, prefix="published_dir")
 
         test_service = { 
             "name": "My Test Service",
@@ -39,11 +39,16 @@ class TestCheckAndDeploy(unittest.TestCase):
         tmp_published_file_name = os.path.basename(self.tmp_published_file[1])
         moved_file = os.path.join(self.tmp_public_dir, tmp_published_file_name)
 
+        tmp_published_folder_name = os.path.basename(self.tmp_published_folder)
+        moved_folder = os.path.join(self.tmp_public_dir, tmp_published_folder_name)
+
         self.assertTrue(os.path.exists(moved_file), "File does not exist")
+        self.assertTrue(os.path.exists(moved_folder), "Folder does not exist")
 
     def test_check_old_files_are_removed_from_public_folder(self):
         
         self.tmp_remove_file = tempfile.mkstemp(dir=self.tmp_public_dir, prefix="remove_")
+        self.tmp_remove_folder = tempfile.mkdtemp(dir=self.tmp_public_dir, prefix="remove_dir_")
 
         test_service = { 
             "name": "My Test Service",
@@ -56,7 +61,11 @@ class TestCheckAndDeploy(unittest.TestCase):
         tmp_removed_file_name = os.path.basename(self.tmp_remove_file[1])
         removed_file = os.path.join(self.tmp_public_dir, tmp_removed_file_name)
 
+        tmp_removed_folder_name = os.path.basename(self.tmp_remove_folder)
+        removed_folder = os.path.join(self.tmp_public_dir, tmp_removed_folder_name)
+
         self.assertFalse(os.path.exists(removed_file), "File should be removed")
+        self.assertFalse(os.path.exists(removed_folder), "Folder should be removed")
 
     def test_check_dotfiles_are_ignored(self):
         
